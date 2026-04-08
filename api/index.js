743ec07d2f5d9dca3aae9e3e5db6ae956127dfd9
kiranyangal1718-ux <kiranyangal1718@gmail.com>
2026-04-08 22:06:50 +0530

    Export app module from index.js
    Export the app module for use in other files.

--- api/index.js
+++ api/index.js
@@ -28,4 +28,4 @@ app.post('/api/translate', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+module.exports = app;
